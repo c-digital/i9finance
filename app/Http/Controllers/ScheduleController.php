d6f7585adfceddd68d25ca1b942f7e0338c1d9cc
Nisa Delgado <nisadelgado@gmail.com>
2022-08-16 23:54:49 -0400

    16-08-22

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Customer;
 use App\Schedule;
+use App\ProductService;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ScheduleController extends Controller
@@ -30,7 +33,31 @@ class ScheduleController extends Controller
 
     public function show(Schedule $schedule)
     {
-        return view('schedule.show', compact('schedule'));
+        $carbon = Carbon::now();
+
+        $hours = [
+            '8:00', '8:15', '8:30', '8:45',
+            '9:00', '9:15', '9:30', '9:45',
+            '10:00', '10:15', '10:30', '10:45',
+            '11:00', '11:15', '11:30', '11:45',
+            '12:00', '12:15', '12:30', '12:45',
+            '13:00', '13:15', '13:30', '13:45',
+            '14:00', '14:15', '14:30', '14:45',
+            '15:00', '15:15', '15:30', '15:45',
+            '16:00', '16:15', '16:30', '16:45',
+            '17:00', '17:15', '17:30', '17:45',
+            '18:00', '18:15', '18:30', '18:45',
+            '19:00', '19:15', '19:30', '19:45',
+            '20:00', '20:15', '20:30', '20:45'
+        ];
+
+        $schedules = Schedule::get();
+
+        $customers = Customer::get();
+
+        $services = ProductService::get();
+
+        return view('schedule.show', compact('carbon', 'schedules', 'schedule', 'hours', 'customers', 'services'));
     }
 
     public function edit($id)
--- resources/views/schedule/show.blade.php
+++ resources/views/schedule/show.blade.php
@@ -8,28 +8,134 @@
     {{ $schedule->name }}
 @endsection
 
-@push('script-page')
-    <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.2/main.js" integrity="sha256-cbWu30RZ0VQ5sBdqcxYa3S3nA+E1d/tkUcflycLQZ50=" crossorigin="anonymous"></script>
+@section('action-button')
+    <div>
+        <select name="calendar" class="form-control">
+            <option value=""></option>
+            @foreach($schedules as $item)
+                <option {{ $schedule->id == $item->id ? 'selected' : '' }} value="{{ $item->id }}">{{ $item->name }}</option>  
+            @endforeach
+        </select>
+    </div>
+@endsection
 
+@push('script-page')
     <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            var calendarEl = document.getElementById('fullcalendar');
-            var calendar = new FullCalendar.Calendar(calendarEl, {
-                initialView: 'dayGridMonth'
+        $(document).ready(function () {
+            $('.cell').click(function () {
+                hour = $(this).attr('data-hour');
+                td = $(this).attr('data-td');
+                td = parseInt(td) - 1;
+                date = $('.date-week').eq(td).text();
+
+                $('#create-event').modal('show');
             });
-            calendar.render();
+
+            $('[name="calendar"]').change(function () {
+                id = $(this).val();
+
+                if (id) {
+                    window.location.href = '/schedule/' + id;
+                }
+            });
         });
     </script>
-@endpush
-
-@push('css-page')
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.2/main.css" integrity="sha256-jLWPhwkAHq1rpueZOKALBno3eKP3m4IMB131kGhAlRQ=" crossorigin="anonymous">
 @endpush
 
 @section('content')
     <div class="row">
         <div class="col-12">
-            <div id="fullcalendar"></div>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th class="text-center"></th>
+
+                        <th class="text-center">
+                            <span class="date-week">{{ $carbon->startOfWeek()->format('d/m/Y') }}</span> <br>
+                            {{ __($carbon->startOfWeek()->format('l')) }}
+                        </th>
+
+                        @for($i = 1; $i <= 6; $i++)
+                            @php
+                                $date = $carbon->addDay(1);
+                            @endphp
+
+                            <th class="text-center">
+                                <span class="date-week">{{ $date->format('d/m/Y') }}</span> <br>
+                                {{ __($date->format('l')) }}
+                            </th>
+                        @endfor
+                    </tr>
+                </thead>
+
+                <tbody>
+                    @foreach($hours as $hour)
+                        <tr>
+                            <td>{{ $hour }}</td>
+                            <td class="text-center cell" data-hour="{{ $hour }}" data-td="1"></td>
+                            <td class="text-center cell" data-hour="{{ $hour }}" data-td="2"></td>
+                            <td class="text-center cell" data-hour="{{ $hour }}" data-td="3"></td>
+                            <td class="text-center cell" data-hour="{{ $hour }}" data-td="4"></td>
+                            <td class="text-center cell" data-hour="{{ $hour }}" data-td="5"></td>
+                            <td class="text-center cell" data-hour="{{ $hour }}" data-td="6"></td>
+                            <td class="text-center cell" data-hour="{{ $hour }}" data-td="7"></td>
+                        </tr>
+                    @endforeach
+                </tbody>       
+            </table>
+        </div>
+    </div>
+
+    <div class="modal" id="create-event" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content" style="background-color: white;">
+                <div class="modal-header">
+                    <h5 class="modal-title">Agregar evento</h5>
+                    
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <div class="modal-body p-4">
+                    <div class="form-group">
+                        <label for="customer_id">{{ __('Customer') }}</label>
+                        <select required name="customer_id" class="form-control select2">
+                            <option value=""></option>
+
+                            @foreach($customers as $customer)
+                                <option value="{{ $customer->id }}">{{ $customer->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="product_id">{{ __('Product') }}</label>
+                        <select required name="product_id" class="form-control select2">
+                            <option value=""></option>
+
+                            @foreach($services as $service)
+                                <option value="{{ $service->id }}">{{ $service->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="date_start">{{ __('Date start') }}</label>
+                        <input required type="datetime-local" name="date_start datepicker" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="date_end">{{ __('Date end') }}</label>
+                        <input required type="datetime-local" name="date_end datepicker" class="form-control">
+                    </div>
+                </div>
+                
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary">Guardar</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
